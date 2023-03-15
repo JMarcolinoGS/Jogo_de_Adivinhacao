03fa161d67ef9d9a129f137a1786c77b662d81be
João Marcolino <122378399+JMarcolinoGS@users.noreply.github.com>
2023-03-15 07:21:01 -0300

    Add files via upload

---
+++ jogo_de_adivinhacao.py
@@ -0,0 +1,34 @@
+import random
+
+print:("Bem vindo ao Jogo!")
+choice_number = input("Dígite um numero para começarmos!:")
+
+if choice_number.isdigit():
+    choice_number = int(choice_number)
+else:
+    print("Erro: valor informado não é numerico. Favor execute novamente e informe um numero")
+    quit()
+
+random_number = random.randint(0,choice_number)
+
+n_choices = 0
+
+while True:
+    answer_user = input("Adivinhe o mumero:")
+    if answer_user.isdigit():
+        answer_user = int(answer_user)
+    else:
+        print("digite um numero")
+        continue
+
+    n_choices = n_choices + 1
+    if answer_user == random_number:
+        print("Acertou o número!")
+        break
+
+    elif answer_user > random_number:
+        print("Chutou alto,tente um número menor")
+    else:
+        print("O numero é maior que isto")
+
+    print("Nº de tentativas:" + str(n_choices))
